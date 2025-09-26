6e36b2b571f7320043ac42ca543ce563926126f2
nularyNasser <nahamed.sveil@gmail.com>
2025-09-26 14:40:50 +0200

    Création de index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    require "libs/function.php";
+    start_page("Tissons Solidaire");
+?>
+
+<body>
+    <h1>Donnez une <span>seconde vie</span> à vos vêtements</h1>
+</body>
+
+<?php end_page() ?>
